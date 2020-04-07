0b7dd454b8505be4344d3f4de40bb3a766c0b7b8
boom100100 <bsd231@nyu.edu>
2020-04-07 17:33:37 -0400

    Added validation to admin model.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
   has_secure_password
-  validates :username, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false, message: 'This username is already taken.' }
   validates :password, confirmation: true
 
 end
