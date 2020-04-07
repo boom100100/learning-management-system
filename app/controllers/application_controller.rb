2bf96c8843d0b55d25a1c8b77c677976b1b0c8b0
boom100100 <bsd231@nyu.edu>
2020-04-06 21:07:18 -0400

    Enabled sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, 'password_security'
+  end
   def index
   end
 end
