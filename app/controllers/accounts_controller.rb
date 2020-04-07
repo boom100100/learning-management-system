35431fb5086c51a4e838440049cb096b98949d15
boom100100 <bsd231@nyu.edu>
2020-04-06 21:06:42 -0400

    Adding signup functionality - incomplete.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,16 @@
-class Accounts_Controller < ApplicationController
+class AccountsController < ApplicationController
   def signup # TODO: determine user type.
+    @admin = Admin.new
+    @teacher = Teacher.new
+    @student = Student.new
+  end
+
+  def create
+    if params[:account_type] == 'student'
+      #redirect_to 'students#create'(request.parameters)
+      #StudentsController.new.create
+
+    end
   end
 
   def login
