e9123fbc98a7a505a7ca4fa7ddab368c0263f45c
boom100100 <bsd231@nyu.edu>
2020-04-07 17:40:10 -0400

    Added show to students controller.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,19 @@
 class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
   def create
     student = Student.create(student_params)
     redirect_to student_path(student) if student
+  end
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find_by(id: params[:id])
   end
 
   private
