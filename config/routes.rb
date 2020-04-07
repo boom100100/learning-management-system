8c69a5ffd92b06b0bc3494b56faa7ebba40e0fc7
boom100100 <bsd231@nyu.edu>
2020-04-07 17:38:59 -0400

    Fixed sessions routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root :to => 'accounts#signup'
-  post '/accounts', to: 'accounts#create'
-  get '/logout', to: 'accounts#logout'
+  root :to => 'sessions#new'
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#logout'
 
   resources :admins
   resources :students
+  resources :teachers
+
+  resources :courses
 end
