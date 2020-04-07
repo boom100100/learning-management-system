fdb3b0d058f59ec1b34df00116d8c84f3076af62
boom100100 <bsd231@nyu.edu>
2020-04-06 21:13:22 -0400

    Edited root. Created route for students.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root :to => 'application#index'
+  root :to => 'accounts#signup'
+  post '/accounts', to: 'accounts#create'
+  get '/logout', to: 'accounts#logout'
 
   resources :admins
+  resources :students
 end
