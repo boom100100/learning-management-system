374ecb7c2d4213ff9c8dd64f615fc812e39e9cdb
boom100100 <bsd231@nyu.edu>
2020-04-08 16:47:37 -0400

    Added crud functionality to teachers.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,18 +3,30 @@ class TeachersController < ApplicationController
     @teachers = Teacher.all
   end
   def new
+    @teacher = Teacher.new
   end
   def create
+    teacher = Teacher.create(teachers_params)
+    teacher ? redirect_to(teachers_path) : redirect_to(new_teacher_path)
   end
+
   def show
+    @teacher = Teacher.find_by(id: params[:id])
   end
   def edit
+    @teacher = Teacher.find_by(id: params[:id])
   end
   def update
+    teacher = Teacher.find_by(id: params[:id])
+    teacher.update(teacher_params)
+    redirect_to teacher
   end
   def destroy
+    teacher = Teacher.find_by(id: params[:id])
+    teacher.destroy
+    redirect_to teachers_path
   end
   private
-  def teachers_params
+  def teacher_params
   end
 end
