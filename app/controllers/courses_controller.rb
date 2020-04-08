4ea1d4cd625c403f8820cc87f7f581d5bb2e9ee9
boom100100 <bsd231@nyu.edu>
2020-04-08 10:32:22 -0400

    Added new, create, show methods to courses controller.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,6 +3,14 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+  end
+
   def show
+    @course = Course.find_by(id: params[:id])
   end
 end
