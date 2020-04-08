2650fe9937513d5d319760c207f7761fb4f0c4c7
boom100100 <bsd231@nyu.edu>
2020-04-08 16:38:21 -0400

    Added skeleton methods for teacher controller.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,19 @@ class TeachersController < ApplicationController
   def index
     @teachers = Teacher.all
   end
+  def new
+  end
+  def create
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+  private
+  def teachers_params
+  end
 end
