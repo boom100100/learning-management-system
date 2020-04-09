c75a3e3532f1f5ca5eba6759bb578c118a797bd9
boom100100 <bsd231@nyu.edu>
2020-04-09 09:49:41 -0400

    Updating courses update method in controller.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,12 +5,16 @@ class CoursesController < ApplicationController
 
   def new # TODO: needs authorization restrictions
     @course = Course.new
+    @teachers = Teacher.all.collect{|element| [element.username, element.id]}
+
   end
 
   def create # TODO: needs authorization restrictions
     # TODO: create failed. course.save! #error: ActiveRecord::RecordInvalid (Validation failed: Teacher must exist, Student must exist)
 
     course = Course.create(course_params)
+    course.teacher = Teacher.find_by(id: params[:course][:teacher])
+    course.save
     if course
       redirect_to courses_path
     else
@@ -24,10 +28,14 @@ class CoursesController < ApplicationController
 
   def edit # TODO: needs authorization restrictions
     @course = Course.find_by(id: params[:id])
+    @teachers = Teacher.all.collect{|element| [element.username, element.id]}
   end
 
   def update # TODO: needs authorization restrictions
     course = Course.find_by(id: params[:id])
+    course.update(course_params)
+    course.teacher = Teacher.find_by(id: params[:course][:teacher_id])
+    course.save
     redirect_to course
   end
 
@@ -40,6 +48,6 @@ class CoursesController < ApplicationController
   private
 
   def course_params
-    params.require(:course).permit(:name, :description, :teacher)
+    params.require(:course).permit(:name, :description)
   end
 end
