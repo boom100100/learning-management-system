4eb95ae3a57c8ef6334308300ed60ad3040b3df3
boom100100 <bsd231@nyu.edu>
2020-04-09 10:28:30 -0400

    Fixed courses tags association in migrations.

---
+++ db/migrate/006_create_lessons.rb
@@ -0,0 +1,15 @@
+class CreateLessons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lessons do |t|
+      t.string :name
+      t.string :description
+      t.string :content
+      t.string :transcript
+      t.string :video_url
+      t.string :dir_url #for shared files
+
+      t.belongs_to :course
+      t.belongs_to :tag
+    end
+  end
+end
