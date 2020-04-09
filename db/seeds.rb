95d7e75df3a0adbee71b67689f084f0d72a38690
boom100100 <bsd231@nyu.edu>
2020-04-09 10:33:38 -0400

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(username: 'a', password: 'password', password_confirmation: 'password')
+Admin.create(username: 'b', password: 'password', password_confirmation: 'password')
+Admin.create(username: 'c', password: 'password', password_confirmation: 'password')
+
+teacher_a = Teacher.create(username: 'a', password: 'a', password_confirmation: 'a')
+Teacher.create(username: 'b', password: 'b', password_confirmation: 'b')
+Teacher.create(username: 'c', password: 'c', password_confirmation: 'c')
+
+Student.create(username: 'a', password: 'a', password_confirmation: 'a')
+Student.create(username: 'b', password: 'b', password_confirmation: 'b')
+Student.create(username: 'c', password: 'c', password_confirmation: 'c')
+
+Course.create(name: 'a', description: 'a', teacher: teacher_a)
+Course.create(name: 'b', description: 'b', teacher: teacher_a)
+Course.create(name: 'c', description: 'c', teacher: teacher_a)
