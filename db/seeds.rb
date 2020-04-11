28b6d3148d512f13bb7641af7a7d08271ca822a4
boom100100 <bsd231@nyu.edu>
2020-04-11 14:58:36 -0400

    Made Lesson :tags into array.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,12 @@ Student.create(username: 'a', password: 'a', password_confirmation: 'a')
 Student.create(username: 'b', password: 'b', password_confirmation: 'b')
 Student.create(username: 'c', password: 'c', password_confirmation: 'c')
 
-Course.create(name: 'a', description: 'a', teacher: teacher_a)
+course_a = Course.create(name: 'a', description: 'a', teacher: teacher_a)
 Course.create(name: 'b', description: 'b', teacher: teacher_a)
 Course.create(name: 'c', description: 'c', teacher: teacher_a)
+
+tag_a = Tag.create([{name: 'a'}, {name: 'b'}, {name: 'c'}]).first
+
+Lesson.create(name: 'a', description: 'a', content: 'a', transcript: 'a', video_url: 'a', dir_url: 'a', course: course_a, tags: [tag_a])
+Lesson.create(name: 'b', description: 'b', content: 'b', transcript: 'b', video_url: 'b', dir_url: 'b', course: course_a, tags: [tag_a])
+Lesson.create(name: 'c', description: 'c', content: 'c', transcript: 'c', video_url: 'c', dir_url: 'c', course: course_a, tags: [tag_a])
