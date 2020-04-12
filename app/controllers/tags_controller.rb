7bbfa01afbe5adb8731f4c9399b44646dcea5fc1
boom100100 <bsd231@nyu.edu>
2020-04-12 14:56:10 -0400

    Added #create, #tag_params to tag controller.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,29 @@
 class TagsController < ApplicationController
   def index
+    @tag = Tag.new
     @tags = Tag.all
   end
 
   def new
     @tag = Tag.new
   end
+
   def show
     @tag = Tag.find_by(id: params[:id])
+    @courses = @tag.courses
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    unless @tag
+      flash[:error] = 'Could not create tag.'
+    end
+    redirect_to tags_path
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
