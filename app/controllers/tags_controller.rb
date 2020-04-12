4f61f6a6c35afd331ee45bee7883d176f25c4097
boom100100 <bsd231@nyu.edu>
2020-04-12 14:37:28 -0400

    Added #index, #new, #show to tags controller.

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,12 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+  def show
+    @tag = Tag.find_by(id: params[:id])
+  end
+end
