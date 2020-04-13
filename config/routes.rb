89ddaa3fe122278745db35dd52624425b3e22fbe
boom100100 <bsd231@nyu.edu>
2020-04-13 17:56:58 -0400

    Added routes for devise. Models: teacher, student, admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,57 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root :to => 'sessions#new'
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#create'
-
-  get '/logout', to: 'sessions#destroy'
 
   resources :admins
+  devise_for :admins, :controllers => {:registrations => "admins/registrations", :sessions => "admins/sessions", :passwords => "admins/passwords" }
+  devise_scope :admin do
+    get "/admins/auth/github/callback" => "admins/omniauth_callbacks#github"
+  end
+  #get '/auth/github'
+  get '/auth/github/callback', to: 'sessions#github_auth'
+  devise_for :teachers, :controllers => {:registrations => "teachers/registrations", :sessions => "teachers/sessions", :passwords => "teachers/passwords" }
+  devise_scope :teacher do
+    #get "/teachers/auth/github"
+    #get "/teachers/auth/github/callback" => "teachers/omniauth_callbacks#github"
+    resources :courses
+  end
+  resources :teachers do
+    resources :courses
+  end
+
+  devise_for :students, :controllers => {:registrations => "teachers/registrations", :sessions => "students/sessions", :passwords => "students/passwords" }
+  devise_scope :student do
+    get "/students/auth/github/callback" => "students/omniauth_callbacks#github"
+  end
 
   resources :students do
     get '/add_course', to: 'students#add_course'
     get '/remove_course', to: 'students#remove_course'
   end
 
-  resources :teachers
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  resources :courses
+
+  # get '/auth/github/callback', to: 'sessions#github_auth'
+  #get '/auth/failure', to: redirect('/')
+  # post '/login', to: 'sessions#create'
+  #
+   get '/logout', to: 'sessions#destroy'
+
+
+
+
+
+  resources :courses do
+    resources :lessons
+    resources :students, only: [:index, :show]
+  end
 
   resources :lessons
   get '/download_files', to: 'lessons#download_dir'
 
-  resources :tags
+  resources :tags do
+    resources :courses, only: [:index, :show]
+  end
 end
