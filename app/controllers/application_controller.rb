63462bf752809d164ef4c0870f6049ac8f6ef1e4
boom100100 <bsd231@nyu.edu>
2020-04-15 17:31:34 -0400

    Overrode devise sign out path for app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    '/'
   end
 end
