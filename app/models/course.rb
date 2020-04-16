168d53948b65c3846f6640b0e443f31facd952cd
boom100100 <bsd231@nyu.edu>
2020-04-16 18:04:43 -0400

    Added scopes revealing published status to models.

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,8 @@ class Course < ApplicationRecord
 
   has_many :course_students
   has_many :students, through: :course_students
+
+  scope :by_status, -> (status) { where('status = ?', status) }
+  scope :published, -> { by_status("public") }
+  scope :drafts, -> { by_status("draft") }
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,8 @@ class Lesson < ApplicationRecord
   has_many :tags, through: :lesson_tags
 
   belongs_to :course
+
+  scope :by_status, -> (status) { where('status = ?', status) }
+  scope :published, -> { by_status("public") }
+  scope :drafts, -> { by_status("draft") }
 end
