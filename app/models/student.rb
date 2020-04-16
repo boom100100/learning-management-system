440f467f33e05e537d15dbafdbe5392766dd4db0
boom100100 <bsd231@nyu.edu>
2020-04-16 18:41:40 -0400

    Fixed associations.

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,7 @@ class Student < ApplicationRecord
   has_many :course_students
   has_many :courses, through: :course_students
   has_many :teachers, through: :courses
-  
+
   validates :email, uniqueness: true
   validates :password, length: { minimum: 6 }
 
