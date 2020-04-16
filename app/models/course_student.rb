abccd2546e9e7952c3aacfe82c53f5199125bce3
boom100100 <bsd231@nyu.edu>
2020-04-16 18:40:50 -0400

    Added association to models for lesson course st udents join.

--- app/models/course_student.rb
+++ app/models/course_student.rb
@@ -1,4 +1,8 @@
 class CourseStudent < ApplicationRecord
   belongs_to :course
   belongs_to :student
+
+  has_many :lessons_course_students
+  has_many :lessons, through: :lessons_course_students
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,6 +4,10 @@ class Lesson < ApplicationRecord
 
   belongs_to :course
 
+  has_many :lessons_course_students
+  has_many :course_students, through: :lessons_course_students
+
+
   scope :by_status, -> (status) { where('status = ?', status) }
   scope :published, -> { by_status("public") }
   scope :drafts, -> { by_status("draft") }
