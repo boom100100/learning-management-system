76a0736dec0af5bdbaaf5d31ab4b5179775d0470
boom100100 <bsd231@nyu.edu>
2020-04-15 23:11:15 -0400

    Added user verification methods to concerns in controller.

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -9,7 +9,7 @@ module Accessible
     if current_admin
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
-      redirect_to(rails_admin.dashboard_path) and return
+      redirect_to(current_admin) and return
     elsif current_teacher
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
@@ -20,16 +20,40 @@ module Accessible
     end
   end
 
+  def direct?(user)
+    unless user
+      redirect_back(fallback_location: root_path)
+      return false
+    end
+    true
+  end
+
   def admin?
-    current_admin ? true : false
+    current_admin
   end
   def teacher?
-    current_teacher ? true : false
+    current_teacher
   end
   def student?
-    current_student ? true : false
+    current_teacher
   end
   def signed_in?
     (admin? || teacher? || student?) ? true : false
+  end
+
+  def visitor_is_self?
+    uri_val = request.original_fullpath.split('/')
+
+    if admin?
+      return (uri_val[1] == 'admins' && current_admin.id == uri_val[2].to_i)
+    elsif teacher?
+      return (uri_val[1] == 'teachers' && current_teacher.id == uri_val[2].to_i)
+    elsif student?
+      return (uri_val[1] == 'students' && current_student.id == uri_val[2].to_i)
+    end
+  end
+
+  def visitor_self_or_admin?
+    visitor_is_self? || admin?
   end
 end
