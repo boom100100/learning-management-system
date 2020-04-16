581f59fa06685af478ed770377cbb41840200985
boom100100 <bsd231@nyu.edu>
2020-04-16 18:38:52 -0400

    Added migration for lesson course students join.

--- db/migrate/005_create_course_students.rb
+++ db/migrate/005_create_course_students.rb
@@ -1,6 +1,8 @@
 class CreateCourseStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :course_students do |t|
+      t.boolean :completed
+      
       t.belongs_to :course
       t.belongs_to :student
     end
---
+++ db/migrate/20200416222848_lessons_course_students.rb
@@ -0,0 +1,10 @@
+class LessonsCourseStudents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lessons_course_students, :id => false do |t|
+      t.boolean :completed
+      
+      t.belongs_to :lesson
+      t.belongs_to :course_students
+    end
+  end
+end
