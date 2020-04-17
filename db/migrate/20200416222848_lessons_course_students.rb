0b276086ddc6eb507e47358a0e741576d0c0f534
boom100100 <bsd231@nyu.edu>
2020-04-17 10:44:22 -0400

    Added migration for lesson course_student associations.

--- db/migrate/20200416222848_lessons_course_students.rb
+++ db/migrate/20200416222848_lessons_course_students.rb
@@ -2,9 +2,9 @@ class LessonsCourseStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :lessons_course_students, :id => false do |t|
       t.boolean :completed
-      
+
       t.belongs_to :lesson
-      t.belongs_to :course_students
+      t.belongs_to :course_student
     end
   end
 end
