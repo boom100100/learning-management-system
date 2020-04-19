dd328777009cd89500dfd3a03ba921c48e0108f1
boom100100 <bsd231@nyu.edu>
2020-04-19 13:51:06 -0400

    Edited seeds to match validation requirements.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,12 @@ Student.create(email: 'a@a.a', password: 'aaaaaa', password_confirmation: 'aaaaa
 Student.create(email: 'b@b.b', password: 'bbbbbb', password_confirmation: 'bbbbbb')
 Student.create(email: 'c@c.c', password: 'cccccc', password_confirmation: 'cccccc')
 
-course_a = Course.create(name: 'a', description: 'a', status: 'public', teacher_id: teacher_a.id)
-Course.create(name: 'b', description: 'b', status: 'public', teacher_id: teacher_a.id)
-Course.create(name: 'c', description: 'c', status: 'draft', teacher_id: teacher_a.id)
+course_a = Course.create(name: 'a', description: 'aaaaaaaaaa', status: 'public', teacher_id: teacher_a.id)
+Course.create(name: 'b', description: 'bbbbbbbbbb', status: 'public', teacher_id: teacher_a.id)
+Course.create(name: 'c', description: 'cccccccccc', status: 'draft', teacher_id: teacher_a.id)
 
 tag_a = Tag.create([{name: 'a'}, {name: 'b'}, {name: 'c'}]).first
 
-Lesson.create(name: 'a', description: 'a', content: 'a', transcript: 'a', video_url: 'a', dir_url: 'a', course: course_a, tags: [tag_a], status: 'public')
-Lesson.create(name: 'b', description: 'b', content: 'b', transcript: 'b', video_url: 'b', dir_url: 'b', course: course_a, tags: [tag_a], status: 'public')
-Lesson.create(name: 'c', description: 'c', content: 'c', transcript: 'c', video_url: 'c', dir_url: 'c', course: course_a, tags: [tag_a], status: 'draft')
+Lesson.create(name: 'a', description: 'aaaaaaaaaa', content: 'aaaaaaaaaa', transcript: 'a', video_url: 'a', course: course_a, tags: [tag_a], status: 'public')
+Lesson.create(name: 'b', description: 'bbbbbbbbbb', content: 'bbbbbbbbbb', transcript: 'b', video_url: 'b', course: course_a, tags: [tag_a], status: 'public')
+Lesson.create(name: 'c', description: 'cccccccccc', content: 'cccccccccc', transcript: 'c', video_url: 'c', course: course_a, tags: [tag_a], status: 'draft')
