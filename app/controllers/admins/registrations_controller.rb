929cd2d46b02bb0319ef6e1ad723c6d76c271516
boom100100 <bsd231@nyu.edu>
2020-04-21 16:34:34 -0400

    Disabled admin signup.

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -7,14 +7,15 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  #disabled #new, #create with empty methods
+  #so registration can't create admins.
   # GET /resource/sign_up
   def new
-    super
+    #super
   end
-
   # POST /resource
   def create
-    super
+    #super
   end
 
   # GET /resource/edit
